9366ca93dfcea8530e7b99fb57171c61a840d557
Федотов Александр Геннадьевич <afedotov@ABZ-1.ru>
2020-09-18 13:49:51 +0300

    Updates

---
+++ precache-manifest.0f2f083b3e384c2af1ab4347dd9e0563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19843dbfe94fc82d1bc82bd1185028b3",
+    "url": "/task_manager/index.html"
+  },
+  {
+    "revision": "f057a4c424a780c47fa2",
+    "url": "/task_manager/static/css/main.933d1deb.chunk.css"
+  },
+  {
+    "revision": "0c4520f4348637ad9376",
+    "url": "/task_manager/static/js/2.5d57d1bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/task_manager/static/js/2.5d57d1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f057a4c424a780c47fa2",
+    "url": "/task_manager/static/js/main.9d361cd9.chunk.js"
+  },
+  {
+    "revision": "1f1f8f6b0063eb8d8fa3",
+    "url": "/task_manager/static/js/runtime-main.6821e0ee.js"
+  }
+]);
